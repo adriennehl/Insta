6035b04d16cee7636e19fe9b68fecb7a0d58a400
Adrienne Li <ahli@fb.com>
2020-07-06 18:00:59 -0500

    server set-up

--- Insta/AppDelegate.m
+++ Insta/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import <Parse/Parse.h>
 
 @interface AppDelegate ()
 
@@ -16,7 +17,15 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
+    
+    ParseClientConfiguration *config = [ParseClientConfiguration   configurationWithBlock:^(id<ParseMutableClientConfiguration> configuration) {
+        
+        configuration.applicationId = @"AL_INSTA_APP";
+        configuration.server = @"https://al-insta-app.herokuapp.com/";
+    }];
+    
+    [Parse initializeWithConfiguration:config];
+    
     return YES;
 }
 
