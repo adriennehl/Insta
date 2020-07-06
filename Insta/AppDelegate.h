7e1477a371184120c0735c18fecdcd41e27c37d4
Adrienne Li <ahli@fb.com>
2020-07-06 15:26:15 -0500

    Initial Commit

---
+++ Insta/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Insta
+//
+//  Created by Adrienne Li on 7/6/20.
+//  Copyright © 2020 ahli. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
