d921518f31d9c9bead4482b05928493b09a5351b
Adrienne Li <ahli@fb.com>
2020-07-07 17:04:35 -0500

    user can take a photo, add a caption, and post

---
+++ Insta/View Controllers/CreatePostViewController.h
@@ -0,0 +1,17 @@
+//
+//  CreatePostViewController.h
+//  Insta
+//
+//  Created by Adrienne Li on 7/6/20.
+//  Copyright © 2020 ahli. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface CreatePostViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Insta/View Controllers/CreatePostViewController.m
@@ -0,0 +1,137 @@
+//
+//  CreatePostViewController.m
+//  Insta
+//
+//  Created by Adrienne Li on 7/6/20.
+//  Copyright © 2020 ahli. All rights reserved.
+//
+
+#import "CreatePostViewController.h"
+#import "Post.h"
+
+@interface CreatePostViewController () <UIImagePickerControllerDelegate, UINavigationControllerDelegate>
+@property (weak, nonatomic) IBOutlet UITextField *captionField;
+@property (weak, nonatomic) IBOutlet UIImageView *photoView;
+@end
+
+@implementation CreatePostViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    
+}
+
+// implement delegate method to take a picture
+- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary<UIImagePickerControllerInfoKey,id> *)info {
+    // get the image captured by UIImagePickerController
+    UIImage *originalImage = info[UIImagePickerControllerOriginalImage];
+    UIImageView *editedImage = info[UIImagePickerControllerEditedImage];
+    CGSize size = CGSizeMake(300, 300);
+    UIImage *resizedImage = [self resizeImage:originalImage withSize:size];
+    
+    // set image
+    [self.photoView setImage:resizedImage];
+    
+    // Dismiss UIImagePickerController and return to original view controller
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+// resize image
+- (UIImage *)resizeImage:(UIImage *)image withSize:(CGSize)size {
+    UIImageView *resizeImageView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, size.width, size.height)];
+    
+    resizeImageView.contentMode = UIViewContentModeScaleAspectFill;
+    resizeImageView.image = image;
+    
+    UIGraphicsBeginImageContext(size);
+    [resizeImageView.layer renderInContext:UIGraphicsGetCurrentContext()];
+    UIImage *newImage = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    
+    return newImage;
+}
+
+- (IBAction)handleTap:(UITapGestureRecognizer *)sender {
+    NSLog(@"Tapped!");
+    // instantiate UIImagePickerController
+    UIImagePickerController *imagePickerVC = [UIImagePickerController new];
+    imagePickerVC.delegate = self;
+    imagePickerVC.allowsEditing = YES;
+    // check that camera is supported on device
+    if ([UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypeCamera]) {
+        imagePickerVC.sourceType = UIImagePickerControllerSourceTypeCamera;
+    }
+    else {
+        NSLog(@"Camera 🚫 available so we will use photo library instead");
+        imagePickerVC.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
+    }
+    
+    [self presentViewController:imagePickerVC animated:YES completion:nil];
+}
+
+- (IBAction)onSave:(id)sender {
+    [Post postUserImage:self.photoView.image withCaption:self.captionField.text withCompletion:^(BOOL succeeded, NSError * _Nullable error) {
+        if (succeeded) {
+            NSLog(@"Posted successfully!");
+            
+            // close tab
+            [self dismissViewControllerAnimated:true completion:nil];
+        }
+        else {
+            UIAlertController *postAlert = [self createAlert:error.localizedDescription title:@"Error Posting"];
+            
+            // show the alert controller
+            [self presentViewController: postAlert animated:YES completion:^{
+                // optional code for what happens after the alert controller has finished presenting
+            }];
+        }
+    }];
+}
+
+- (IBAction)onClose:(id)sender {
+    [self dismissViewControllerAnimated:true completion:nil];
+}
+
+// given an error description and title, return an alert with cancel and ok buttons
+- (UIAlertController *) createAlert: (NSString *) errorDescription title: (NSString *) title {
+    
+    
+    // create a cancel action
+    UIAlertAction *cancelAction = [UIAlertAction actionWithTitle:@"Cancel"
+                                                           style:UIAlertActionStyleCancel
+                                                         handler:^(UIAlertAction * _Nonnull action) {
+        // handle cancel response here. Doing nothing will dismiss the view.
+    }];
+    
+    // create an OK action
+    UIAlertAction *okAction = [UIAlertAction actionWithTitle:@"OK"
+                                                       style:UIAlertActionStyleDefault
+                                                     handler:^(UIAlertAction * _Nonnull action) {
+        // handle response here.
+    }];
+    
+    UIAlertController *alert = [UIAlertController alertControllerWithTitle: title
+                                                                   message:errorDescription
+                                                            preferredStyle:(UIAlertControllerStyleAlert)];
+    
+    // add the cancel action to the alertController
+    [alert addAction:cancelAction];
+    
+    // add the OK action to the alert controller
+    [alert addAction:okAction];
+    
+    return alert;
+}
+
+/*
+ #pragma mark - Navigation
+ 
+ // In a storyboard-based application, you will often want to do a little preparation before navigation
+ - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+ // Get the new view controller using [segue destinationViewController].
+ // Pass the selected object to the new view controller.
+ }
+ */
+
+@end
